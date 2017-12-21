ab73ee9f92f20b3dbe419b70bbdf2caef768b3b1
rkaranja <rkaranja@cytonn.com>
2017-12-21 11:02:36 +0300

    Styling left navigation menu

---
+++ resources/assets/js/components/home/home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="home-backgound">
+    <div class="grid-x">
+      <div class="small-2 cell left-nav">
+        <div class="ovetop">
+          <h4>Store App</h4>
+          <ul class="nav">
+            <li>
+              <a>
+                <i class="el-icon-menu"></i>
+                <span>Dashboard</span>
+              </a>
+            </li>
+
+            <li>
+              <a>
+                <i class="el-icon-circle-plus"></i>
+                <span>Add Item</span>
+              </a>
+            </li>
+
+            <li>
+              <a>
+                <i class="el-icon-info"></i>
+                <span>View Items</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div class="small-10 cell home-body">
+        <div class="mynav">
+          <h2>Store Application Example</h2>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name:'home'
+}
+</script>
--- resources/assets/js/router/index.js
+++ resources/assets/js/router/index.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Welcome from '../components/welcome'
 import Login from '../components/membership/login'
+import Home from '../components/home/home'
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
+
     routes: [
       {
         path: '/',
@@ -14,6 +16,10 @@ const router = new VueRouter({
       {
         path: '/login',
         component: Login
+      },
+      {
+        path: '/home',
+        component: Home
       }
     ]
 });

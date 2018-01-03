f5c20e0e89163a896363a8b0f4c88b188b4f4396
rkaranja <rkaranja@cytonn.com>
2018-01-03 16:57:36 +0300

    Save items added from a form

--- resources/assets/js/components/items/AddItem.vue
+++ resources/assets/js/components/items/AddItem.vue
@@ -1,22 +1,28 @@
 <script>
+
 export default {
   name: 'addItem',
 
-  data(){
-    return {
-      addItem: {
+storage: {
+  data:{
+
+      items: {
         name: '',
         category: '',
         quantity: '',
         price: '',
         description: ''
       }
+
+  }
+},
+  methods: {
+    registerItem(){
+      this.$store.dispatch('addItem', this.items)
     }
   },
-  methods: {
-    addItem(){
+  computed: {
 
-    }
   }
 }
 </script>
@@ -29,12 +35,12 @@ export default {
         <div class="grid-x grid-padding-x">
           <div class="medium-6 cell">
             <label>Item Name
-              <input v-model="addItem.name" type="text" placeholder="Item Name">
+              <input v-model="items.name" type="text" placeholder="Item Name">
             </label>
           </div>
           <div class="medium-6 cell">
             <label>Input Category
-              <input v-model="addItem.category" type="text" placeholder="Item Category">
+              <input v-model="items.category" type="text" placeholder="Item Category">
             </label>
           </div>
         </div>
@@ -42,12 +48,12 @@ export default {
         <div class="grid-x grid-padding-x">
           <div class="medium-6 cell">
             <label>Item Quantity
-              <input v-model="addItem.quantity" type="text" placeholder="Item Quantity">
+              <input v-model="items.quantity" type="text" placeholder="Item Quantity">
             </label>
           </div>
           <div class="medium-6 cell">
             <label>Input Price
-              <input v-model="addItem.price" type="text" placeholder="Input Price">
+              <input v-model="items.price" type="text" placeholder="Input Price">
             </label>
           </div>
         </div>
@@ -55,14 +61,14 @@ export default {
         <div class="grid-x grid-padding-x">
           <div class="medium-12 cell">
             <label>Item Description
-              <textarea v-model="addItem.description" placeholder="Item Description" rows="6"></textarea>
+              <textarea v-model="items.description" placeholder="Item Description" rows="6"></textarea>
             </label>
           </div>
         </div>
 
         <div class="grid-x grid-padding-x">
           <div class="medium-12 cell">
-            <el-button type="success" @click="addItem()" >Add Item To Store Directory</el-button>
+            <el-button type="success" @click="registerItem" >Add Item To Store Directory</el-button>
           </div>
         </div>
 

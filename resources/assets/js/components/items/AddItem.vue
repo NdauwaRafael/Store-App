c259d70054c97ab7aa801f22a9ccd07eaa0557a4
rkaranja <rkaranja@cytonn.com>
2018-01-03 12:40:34 +0300

    Adding model to various add Item form fields and binding them with data models

---
+++ resources/assets/js/components/items/AddItem.vue
@@ -0,0 +1,77 @@
+<script>
+export default {
+  name: 'addItem',
+
+  data(){
+    return {
+      addItem: {
+        name: '',
+        category: '',
+        quantity: '',
+        price: '',
+        description: ''
+      }
+    }
+  },
+  methods: {
+    addItem(){
+
+    }
+  }
+}
+</script>
+
+
+<template >
+  <div class="add-item">
+    <form>
+        <div class="grid-container">
+        <div class="grid-x grid-padding-x">
+          <div class="medium-6 cell">
+            <label>Item Name
+              <input v-model="addItem.name" type="text" placeholder="Item Name">
+            </label>
+          </div>
+          <div class="medium-6 cell">
+            <label>Input Category
+              <input v-model="addItem.category" type="text" placeholder="Item Category">
+            </label>
+          </div>
+        </div>
+
+        <div class="grid-x grid-padding-x">
+          <div class="medium-6 cell">
+            <label>Item Quantity
+              <input v-model="addItem.quantity" type="text" placeholder="Item Quantity">
+            </label>
+          </div>
+          <div class="medium-6 cell">
+            <label>Input Price
+              <input v-model="addItem.price" type="text" placeholder="Input Price">
+            </label>
+          </div>
+        </div>
+
+        <div class="grid-x grid-padding-x">
+          <div class="medium-12 cell">
+            <label>Item Description
+              <textarea v-model="addItem.description" placeholder="Item Description" rows="6"></textarea>
+            </label>
+          </div>
+        </div>
+
+        <div class="grid-x grid-padding-x">
+          <div class="medium-12 cell">
+            <el-button type="success" @click="addItem()" >Add Item To Store Directory</el-button>
+          </div>
+        </div>
+
+        </div>
+        </form>
+  </div>
+</template>
+
+
+
+<style>
+</style>
---
+++ resources/assets/js/components/items/ItemList.vue
@@ -0,0 +1,89 @@
+<script>
+  export default {
+    name: 'itemList',
+    data() {
+      return {
+        tableData: [{
+          date: '2016-05-03',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+          tag: 'Home'
+        }, {
+          date: '2016-05-02',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+          tag: 'Office'
+        }, {
+          date: '2016-05-04',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+          tag: 'Home'
+        }, {
+          date: '2016-05-01',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+          tag: 'Office'
+        }]
+      }
+    },
+    methods: {
+      formatter(row, column) {
+        return row.address;
+      },
+      filterTag(value, row) {
+        return row.tag === value;
+      }
+    }
+  }
+</script>
+
+<template>
+  <div class="grid-container grid-padding-x">
+    <el-table
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="Date Added"
+        sortable
+        width="180">
+      </el-table-column>
+
+      <el-table-column
+        prop="name"
+        label="Added By"
+        width="180">
+      </el-table-column>
+
+      <el-table-column
+        prop="address"
+        label="Item Name"
+        :formatter="formatter">
+      </el-table-column>
+
+      <el-table-column
+        prop="tag"
+        label="Edit"
+        width="100"
+          >
+        <template slot-scope="scope">
+          <el-tag type="info">Edit</el-tag>
+        </template>
+
+      </el-table-column>
+
+      <el-table-column
+        prop="tag"
+        label="Delete"
+        width="100"
+          >
+        <template slot-scope="scope">
+          <el-tag type="danger">Delete</el-tag>
+        </template>
+
+      </el-table-column>
+
+    </el-table-column>
+    </el-table>
+  </div>
+</template>

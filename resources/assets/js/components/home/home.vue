4db8e9da5580817c6e83da8ff6e1eed481dc9b75
rkaranja <rkaranja@cytonn.com>
2018-01-03 12:38:24 +0300

    Styling home page and adding various homepage aspects

--- resources/assets/js/components/home/home.vue
+++ resources/assets/js/components/home/home.vue
@@ -4,26 +4,43 @@
       <div class="small-2 cell left-nav">
         <div class="ovetop">
           <h4>Store App</h4>
-          <ul class="nav">
+          <ul >
             <li>
-              <a>
+              <a >
                 <i class="el-icon-menu"></i>
                 <span>Dashboard</span>
               </a>
             </li>
 
             <li>
-              <a>
-                <i class="el-icon-circle-plus"></i>
-                <span>Add Item</span>
-              </a>
+
+                <router-link :to="{ name: 'addItem'}">
+                  <a>
+                  <i class="el-icon-circle-plus"></i>
+                  <span>Add Item</span>
+                  </a>
+                </router-link>
+
             </li>
 
             <li>
-              <a>
-                <i class="el-icon-info"></i>
-                <span>View Items</span>
-              </a>
+              <router-link :to="{ name: 'itemList' }">
+                <a>
+                  <i class="el-icon-info"></i>
+                  <span>View Items</span>
+                </a>
+              </router-link>
+
+            </li>
+
+            <li>
+              <router-link :to="{ name: 'itemList' }">
+                <a>
+                  <i class="el-icon-info"></i>
+                  <span>Add Store Manager</span>
+                </a>
+              </router-link>
+
             </li>
           </ul>
         </div>
@@ -32,8 +49,27 @@
 
       <div class="small-10 cell home-body">
         <div class="mynav">
-          <h2>Store Application Example</h2>
+          <div class="grid-container">
+            <div class="grid-x">
+              <div class="small-8">
+              </div>
+
+              <div class="small-4 left-top-home">
+                <div class="input-group search">
+                  <input class="input-group-field" type="text">
+                  <div class="input-group-button">
+                    <el-button type="primary" icon="el-icon-search">Search</el-button>
+                  </div>
+                </div>
+
+                <div class="settings">
+                  <i class="el-icon-setting"></i>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+        <router-view></router-view>
       </div>
     </div>
 
